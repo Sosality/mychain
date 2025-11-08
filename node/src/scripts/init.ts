8eeba68309118448c0335d8553f81d0865a18b64
Sosality <96949675+Sosality@users.noreply.github.com>
2025-11-08 23:09:00 +0300

    Delete node directory

--- node/src/scripts/init.ts
+++
@@ -1,7 +0,0 @@
-import { simpleChain } from '../blockchain';
-
-async function main(){
-  await simpleChain.createGenesis();
-  console.log('Genesis created');
-}
-main().catch(e=>{console.error(e); process.exit(1)});
